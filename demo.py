1c1ecb82dc642b75f5bc482326199bf31ce26ad2
remote-cto <nitesh@remotecto.in>
2024-12-24 16:16:02 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+client = OpenAI(api_key="your api key")
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)
